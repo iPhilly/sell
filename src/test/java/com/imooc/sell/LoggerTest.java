871a008e8311872d1d8b37fd687c6e76063d6ee6
iPhilly <liufei.hehe@qq.com>
2017-09-25 16:59:59 +0800

    Initial Commit fr local

---
+++ src/test/java/com/imooc/sell/LoggerTest.java
@@ -0,0 +1,24 @@
+package com.imooc.sell;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoggerTest {
+
+    @Test
+    public void test1() {
+
+        Logger logger = LoggerFactory.getLogger(LoggerTest.class);
+
+        logger.debug("debug..."); // 打印等级限制,终端无法显示打印
+        logger.info("info...");
+        logger.error("error...");
+    }
+}
